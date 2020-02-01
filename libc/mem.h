b6f321178f4dc646d27e500e5060a04821ae66ed
Andrew Schlusberg <aschlus@optonline.net>
2020-02-01 02:27:33 -0500

    Stack implemented

--- libc/mem.h
+++ libc/mem.h
@@ -1,10 +1,9 @@
-#ifndef UTIL_H
-#define UTIL_H
+#ifndef MEM_H
+#define MEM_H
 
 #include "../cpu/types.h"
 
 void memory_copy(char *source, char *dest, int nbytes);
 void memory_set(u8 *dest, u8 val, u32 len);
-void int_to_ascii(int n, char str[]);
 
 #endif
